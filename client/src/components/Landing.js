378565ec1103b000b446cff6d6bf32cb4dfb492e
albisk96 <albertas.kruzintaitis24@gmail.com>
2020-09-10 13:05:31 +0300

    Initial commit

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        Emaily!
+      </h1>
+      Collect feedback form your users. Click on the Emaily logo to see the surveys.
+    </div>
+  );
+};
+
+export default Landing;
